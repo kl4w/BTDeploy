b62f7d986bf37aa8a202c45892a50a42f7a6cc3d
Ka Shing Law <ka.shing.law@gmail.com>
2013-08-13 10:38:36 +1000

    Fix breaking tests. woo hoo.

--- BTDeploy.Tests/TorrentClientBase.cs
+++ BTDeploy.Tests/TorrentClientBase.cs
@@ -3,20 +3,32 @@ using NUnit.Framework;
 using BTDeploy.ServiceDaemon.TorrentClients;
 using System.IO.Abstractions.TestingHelpers;
 using System.Collections.Generic;
+using Moq;
+using System.IO;
 
 namespace BTDeploy.Tests
 {
 	public abstract class TorrentClientBase
 	{
-		public MonoTorrentClient TorrentClient;
+		public ITorrentClient TorrentClient;
 
 		[SetUp]
 		public void SetUp()
 		{
-			var fileSystem = new MockFileSystem (new Dictionary<string, MockFileData> {
-				{ @"C:\foo\bar\test.txt", new MockFileData("hello world.") }
-			});
-			TorrentClient = new MonoTorrentClient ("~/appdata", fileSystem);
+			var fileSystem = new MockFileSystem ();
+			var mockedTorrentClient = new Mock<ITorrentClient> () { CallBase = true };
+			var torrentDetails = new TorrentDetails {
+				Id = "testId",
+				Name = "testName",
+				Files = new string[] {},
+				OutputDirectory = "testOutputDirectory",
+				Status = TorrentStatus.Seeding
+			};
+			mockedTorrentClient.Setup (c => c.List ()).Returns (new ITorrentDetails[] { torrentDetails });
+			mockedTorrentClient.Setup (c => c.FileSystem).Returns (fileSystem);
+			mockedTorrentClient.Setup (c => c.Add (It.IsAny<Stream> (), It.IsAny<string> ()))
+				.Callback<Stream, string> ((t, o) => fileSystem.AddFile (o, new MockFileData("blah.")));
+			TorrentClient = mockedTorrentClient.Object;
 		}
 	}
 }
